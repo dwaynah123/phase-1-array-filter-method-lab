f0b44c02410ff3be2e3e13695958363b9f32225b
Dwayna Harris <dwaynaharris@gmail.com>
2023-05-06 06:50:34 -0400

    practice using filter method to make functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    let found = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+    return found;
+}
+
+function fuzzyMatch(drivers, str) {
+    let found = drivers.filter(driver => driver.charAt(0) === str.charAt(0));
+    return found;
+}
+
+function matchName(driversObj, str) {
+    let found = driversObj.filter(driver => driver.name === str);
+    return found;
+}
+
